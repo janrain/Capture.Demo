0b5a884797957ddb3e77b3e372da2740bed235a9
Vincent Charles <vjcharles@gmail.com>
2011-07-25 10:24:31 -0700

    Removed resize function for edit profile page

--- editprofile.php
+++ editprofile.php
@@ -32,14 +32,6 @@ else {
 
 <script type='text/javascript'>
   var CAPTURE = {};
-
-  CAPTURE.resize = function(json) {
-    var o = JSON.parse(json);
-    $("iframe").height(o.h).width(o.w);
-    if (typeof console !== 'undefined') {
-      console.log("resize", o);
-    }
-  };
   CAPTURE.closeProfileEditor = function() {
     window.location = ".";
   };
